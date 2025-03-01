7dac3695c2b18ea3c112b125fed3d8d991a197c0
Hoang Huy Duc <huyduc1602@gmail.com>
2025-03-02 03:30:31 +0700

    Add GitHub Actions workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import * as path from 'path'
 import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
+import react from '@vitejs/plugin-react'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/interview/' : '/',
+    plugins: [react()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src')
-        }
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-    // ...rest of your Vite config
 })
