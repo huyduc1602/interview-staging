b2812d3777ae8a1c6cd54ea150bb585b99380ad5
Hoang Huy Duc <huyduc1602@gmail.com>
2025-03-02 16:20:46 +0700

    Remove unused header logic from Layout component; simplify layout structure

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,3 @@
-import { useLocation } from 'react-router-dom';
 import { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -6,18 +5,9 @@ interface LayoutProps {
 }
 
 export function Layout({ children }: LayoutProps) {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
 
   return (
     <div className="min-h-screen bg-gray-50">
-      {!isHome && (
-        <header className="sticky top-0 z-50 bg-white border-b">
-          <div className="container-fluid h-16 flex items-center">
-            {/* Header content */}
-          </div>
-        </header>
-      )}
       <main className="flex-1">
         {children}
       </main>
