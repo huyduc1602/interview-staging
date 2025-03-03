b6817fdbb0d1c535eb6bebab822c444c0aa6e6c9
Hoang Huy Duc <huyduc1602@gmail.com>
2025-03-03 19:32:07 +0700

    Add new icon components for AlertCircle and Save; refactor User component

--- src/components/icons/User.tsx
+++ src/components/icons/User.tsx
@@ -4,27 +4,6 @@ export interface LucideProps extends React.SVGProps<SVGSVGElement> {
     size?: string | number;
 }
 
-const AlertCircle: React.FC<LucideProps> = ({ size = 24, ...props }) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        {...props}
-    >
-        <circle cx="12" cy="12" r="10" />
-        <line x1="12" y1="8" x2="12" y2="12" />
-        <line x1="12" y1="16" x2="12.01" y2="16" />
-    </svg>
-);
-
-export default AlertCircle;
-
 const User: React.FC<LucideProps> = ({ size = 24, ...props }) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -44,24 +23,3 @@ const User: React.FC<LucideProps> = ({ size = 24, ...props }) => (
 );
 
 export default User;
-
-const Save: React.FC<LucideProps> = ({ size = 24, ...props }) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={size}
-        height={size}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        {...props}
-    >
-        <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
-        <polyline points="17 21 17 13 7 13 7 21" />
-        <polyline points="7 3 7 8 15 8" />
-    </svg>
-);
-
-export default Save;
