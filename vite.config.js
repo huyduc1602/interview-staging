7dc8fc87b37a174bc7f5df0a0f3c10f3739f3875
Hoang Huy Duc <huyduc1602@gmail.com>
2025-03-03 12:52:07 +0700

    Update Vite configuration base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/interview.hoanghuyduc.com/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
