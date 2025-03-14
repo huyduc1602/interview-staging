32daf1a79ebe32f98d72304a2d6552977b5df638
Hoang Huy Duc <huyduc1602@gmail.com>
2025-03-15 02:08:07 +0700

    Add GitHub Client ID to environment variables and update AuthService to use it

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,9 @@ interface ImportMetaEnv {
     readonly VITE_GOOGLE_SHEET_API_KEY: string
     readonly VITE_SPREADSHEET_ID: string
     readonly VITE_OPENCHAT_API_KEY: string
+    readonly VITE_GITHUB_CLIENT_ID: string
+    readonly VITE_SUPABASE_ANON_KEY: string
+    readonly VITE_SUPABASE_URL: string
 }
 
 interface ImportMeta {
