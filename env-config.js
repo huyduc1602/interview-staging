8ba00c9d3b614782f76f79dbd1f286fcf95cd8d7
huyduc1602 <huyduc1602@users.noreply.github.com>
2025-03-16 22:38:07 +0000

    deploy: b92cacb6d1bf20194e3cd8bdf8d59c746557edb3

---
+++ env-config.js
@@ -0,0 +1,6 @@
+// This file is dynamically generated during build
+// It exposes environment variables to the browser
+globalThis.__ENV = globalThis.__ENV || {};
+
+// Parse homepage from package.json during build
+globalThis.__ENV.CUSTOM_DOMAIN = "%CUSTOM_DOMAIN%"; // Will be replaced during build
